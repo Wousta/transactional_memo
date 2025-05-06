9c7397d634527c5af8bb28f28cf04835a7613562
LuisBMI <luisbmi55@gmail.com>
2025-05-06 15:55:50 +0200

    Add initial implementation of transaction management system with LinkedList and Region classes

---
+++ include/LinkedList.h
@@ -0,0 +1,48 @@
+#ifndef CS453_2024_PROJECT_MASTER_LINKEDLIST_H
+#define CS453_2024_PROJECT_MASTER_LINKEDLIST_H
+
+#include <stdint.h>
+#include <cstdlib>
+#include <string.h>
+#include "macros.h"
+
+struct Node {
+     Node(void *address, void *val, size_t val_size)
+        : address(address), val(nullptr), next(nullptr) {
+        if(val) {
+            this->val = new uint8_t[val_size];
+            memcpy(this->val, val, val_size);
+        }
+        else {
+            this->val = nullptr;
+        }
+    }
+
+    ~Node() {
+        delete[] static_cast<uint8_t*>(val);
+    }
+
+    void *address;      // the lock and location address are related so we need to keep only one of them in the read-set.
+    void *val;          // Only used in the write-set
+    struct Node* next;
+};
+
+class LinkedList {
+    private:
+        Node *head;
+        Node *tail;
+
+    public:
+        LinkedList();
+        ~LinkedList();
+
+        Node *getHead() { return head; }
+        Node *getTail() { return tail; }
+
+        void add(Node *node);
+        // No remove, not necessary in this implementation
+        Node *get(void *address);
+};
+
+
+#endif //CS453_2024_PROJECT_MASTER_LINKEDLIST_H
---
+++ include/Region.h
@@ -0,0 +1,52 @@
+#ifndef CS453_2024_PROJECT_MASTER_REGION_H
+#define CS453_2024_PROJECT_MASTER_REGION_H
+
+#include <mutex>
+#include "VersionSpinLock.h"
+#include "glob_constants.h"
+
+/**
+ * @brief List of dynamically allocated segments.
+ */
+struct segment_node {
+    struct segment_node* prev;
+    struct segment_node* next;
+    // uint8_t segment[] // segment of dynamic size
+};
+
+typedef struct segment_node* segment_list;
+
+class Region {
+    private:
+        void* start;
+        segment_list allocs;
+        VersionSpinLock locks[LOCK_ARRAY_SIZE];
+        std::mutex segmentListMutex;
+        std::atomic_uint clock;
+
+    public:
+        const size_t size;
+        const size_t align;
+        std::atomic_uint current_txs = 0;
+
+
+        Region(size_t size, size_t align);
+
+        ~Region();
+
+        void* getStart() { return start; }
+        segment_list getAllocs() { return allocs; }
+        void setAllocs(segment_list allocs) { this->allocs = allocs; }
+        void unlockSegmentList() { segmentListMutex.unlock(); }
+        void lockSegmentList() { segmentListMutex.lock(); }
+        int getClockVersion() { return clock.load(); }
+        int incrementClockVersion() { return clock.fetch_add(1) + 1; }
+
+        VersionSpinLock* getSpinLocks() { return locks; }
+        int getSpinLockState(int index) { return versionSpinLock_get_state(&locks[index]); }
+        bool acquireSpinLock(int index) { return versionSpinLock_acquire(&locks[index]); }
+        void releaseSpinLock(int index) { versionSpinLock_release(&locks[index]); }
+};
+
+
+#endif //CS453_2024_PROJECT_MASTER_REGION_H
